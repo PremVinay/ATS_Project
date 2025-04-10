9c7ee6925f1177c1b470a1adcb9e105df7af752d
PremVinay <premvinayreddy666@gmail.com>
2025-04-10 18:05:39 +0530

    HomePage Creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { RegistrationformComponent } from './registrationform/registrationform.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 export const routes: Routes = [
+    {path:"" ,component:HomepageComponent},
     {path:"registration-form" ,component:RegistrationformComponent}, 
+
 ];
